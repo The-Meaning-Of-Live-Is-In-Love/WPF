e25a39516efdb1fb167ffa7407293fbcbec69566
TMOLIIL <themeaningofliveisinlove@gmail.com>
2026-03-21 12:10:17 +0300

    Add files via upload

--- Game/CS/MainWindow.xaml.cs
+++ Game/CS/MainWindow.xaml.cs
@@ -23,6 +23,15 @@ namespace Game
         }
         int Shift_Position_X;
         int Shift_Position_Y;
+        int Score_Point_Shift_Position_X = 0;
+        int Score_Point_Shift_Position_Y = 0;
+        private void Generation_Random_Position()
+        {
+            Random Random_Value_1 = new Random();
+            Score_Point_Shift_Position_X = Random_Value_1.Next(-960, 960);
+            Random Random_Value_2 = new Random();
+            Score_Point_Shift_Position_Y = Random_Value_2.Next(-540, 540);
+        }
         private void Movement(object sender, KeyEventArgs Key_Pressed)
         {
             if (Key_Pressed.Key == Key.S)
@@ -60,12 +69,19 @@ namespace Game
         }
         private void Checking_Position(int Shift_Position_X, int Shift_Position_Y)
         {
-            if ((Shift_Position_X > -15) && (Shift_Position_X < 15) && (Shift_Position_Y > 85) && (Shift_Position_Y < 115))
+            if ((Shift_Position_X > Score_Point_Shift_Position_X - 15) && (Shift_Position_X < Score_Point_Shift_Position_X + 15) && (Shift_Position_Y > Score_Point_Shift_Position_Y - 15) && (Shift_Position_Y < Score_Point_Shift_Position_Y + 15))
             {
                 Score_Point.Opacity = 1;
             }
             else
             {
+                Score_Point.Opacity = 0;
+            }
+            if ((Shift_Position_X > Score_Point_Shift_Position_X - 5) && (Shift_Position_X < Score_Point_Shift_Position_X + 5) && (Shift_Position_Y > Score_Point_Shift_Position_Y - 5) && (Shift_Position_Y < Score_Point_Shift_Position_Y + 5))
+            {
+                Generation_Random_Position();
+                Canvas.SetLeft(Score_Point, Score_Point_Shift_Position_X);
+                Canvas.SetTop(Score_Point, Score_Point_Shift_Position_Y);
                 Score_Point.Opacity = 0;
             }
         }
