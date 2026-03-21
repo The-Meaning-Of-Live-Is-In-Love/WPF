d619bc430e226588d5939fab455816c06cb364b5
TMOLIIL <themeaningofliveisinlove@gmail.com>
2026-03-21 11:16:53 +0300

    Add files via upload

---
+++ Input_Output_Application/CS/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace InputOutputApplication
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Binding binding = new Binding();
+            binding.ElementName = "Information_Message_Box";
+            binding.Path = new PropertyPath("Text");
+            Information_Message_Block.SetBinding(TextBlock.TextProperty, binding);
+        }
+    }
+}
